4bcf62ee7208c2e2b3ef55f35e8130fea8ba3849
pradeep-iitb <pradeepkumawat132005@gmail.com>
2025-08-17 03:34:02 -0400

    js file

---
+++ script.js
@@ -0,0 +1,27 @@
+let btn1 = document.querySelector(#btn1)
+  let btn2 = document.querySelector(#btn2)
+  let btn3 = document.querySelector(#btn3)
+  let btn4 = document.querySelector(#btn4)
+  let btn5 = document.querySelector(#btn5)
+
+  function show(i) { 
+    for (let count = 1 ; count < 6 ; count ++)
+    if ( count === i ) {
+        let note= #note+count ;
+        console.log (note);
+        let noteVal = document.querySelector(note) ;
+        noteVal.style.display = block ;
+    } else {
+        let note= #note+count ;
+        console.log(note);
+        let noteVal = document.querySelector(note) ;
+        noteVal.style.display = none ;
+    }
+  }
+
+
+  btn1.addEventListener(click,() => {show(1);})
+  btn2.addEventListener(click,() => {show(2);})
+  btn3.addEventListener(click,() => {show(3);})
+  btn4.addEventListener(click,() => {show(4);})
+  btn5.addEventListener(click,() => {show(5);})
