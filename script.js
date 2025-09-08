54823f4e4e24a5b6813da73022d39d492ff15698
pradeep-iitb <pradeepkumawat132005@gmail.com>
2025-09-08 22:26:20 +0530

    uu

--- script.js
+++ script.js
@@ -25,3 +25,34 @@
   btn4.addEventListener("click",() => {show(4);})
   btn5.addEventListener("click",() => {show(5);})
   btn6.addEventListener("click",() => {show(6);})
+
+  document.querySelector("#menuDropdownText").addEventListener("click" , () => {
+    let menu= document.querySelector("#embedPdf");
+    let time = document.querySelector(".time");
+    if (window.getComputedStyle(time).display == "block") {
+      console.log(menu.style.display);
+      menu.style.display = "block";
+      time.style.display = "none" ;
+      console.log("menu displayed");
+    }
+    else{
+      console.log(menu.style.display);
+      menu.style.display = "none";
+      time.style.display = "block" ;
+      console.log("menu hidden");
+    }
+  })
+
+  document.querySelector("#menuDropdownText").addEventListener("click" , () => {
+    let backward= document.querySelector("#backward");
+    let forward = document.querySelector("#forward");
+    if (window.getComputedStyle(forward).display == "block") {
+      console.log(window.getComputedStyle(forward).display) ;
+      backward.style.display = "block";
+      forward.style.display = "none" ;
+    }
+    else{
+      backward.style.display = "none";
+      forward.style.display = "block" ;
+    }
+  })
